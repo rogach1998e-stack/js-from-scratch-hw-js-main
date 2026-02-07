259b334c2bb336f3e877a904d5e9d94d5d0ec529
Evgen Rog <rogach1998e@gmail.com>
2026-02-07 12:39:23 +0300

    Lesson2JS03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -7,15 +7,15 @@
 const score = 70 // тестовое значение, можно изменять
 let grade
 // your code
-if (score < 0) {
+if (score <= 49) {
 	grade = 'F'
-} else if (score <= 49) {
-	grade = 'D'
 } else if (score <= 69) {
+	grade = 'D'
+} else if (score <= 79) {
 	grade = 'C'
 } else if (score <= 89) {
 	grade = 'B'
 } else if (score <= 100) {
 	grade = 'A'
 }
-console.log('Оценка студента', score);
+console.log('Оценка студента', grade);
