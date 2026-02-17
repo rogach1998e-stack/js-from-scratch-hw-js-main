10306965ac4351464a0950404c4f622307876d6d
Evgen Rog <rogach1998e@gmail.com>
2026-02-17 19:17:25 +0300

    4JS

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,13 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+	let uniqueArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!includesElement(uniqueArray, array[i])) {
+            uniqueArray.push(array[i]);
+        }
+    }
+    return uniqueArray;
+}
+console.log(findUniqueElements([1, 2, 3, 2, 2, 4, 3]));
