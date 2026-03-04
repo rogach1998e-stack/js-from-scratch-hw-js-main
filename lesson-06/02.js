955ab725567eed956384301f5be4ca1b793706a5
Evgen Rog <rogach1998e@gmail.com>
2026-03-04 18:58:05 +0300

    JS6

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,13 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const result = [];
+  for (let i = 0; i < array.lenght; i++) {
+    const newValue = callback(array[i], i);
+      result.push(newValue);
+    }
+  
+  return result;
+}
+
