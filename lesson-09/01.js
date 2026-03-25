1fc544048b8211cbbbd050470b128ab6d14f8743
Evgen Rog <rogach1998e@gmail.com>
2026-03-25 20:08:31 +0300

    JS9

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -68,3 +68,19 @@ clearCartButton.addEventListener('click', function () {
 })
 
 // Твой код:
+petShop.addEventListener('click', function (event) {
+  if (event.target.classList.contains('pet')) {
+    
+    messageBox.textContent = '';
+
+    if (cart.length < 3) {
+      const petId = event.target.id;
+      
+      cart.push(petId);
+      
+      updateCartDisplay();
+    } else {
+      messageBox.textContent = 'Вы не можете добавить более 3 питомцев';
+    }
+  }
+});
